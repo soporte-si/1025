98c23af5d4b7e37f2581ce5dd8002700be96e855
soporte-ux <162223886+soporte-ux@users.noreply.github.com>
2024-03-06 10:48:10 -0600

    add files nuevo home

---
+++ santa/js/santaapp.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.usuario.value,
+            "Clave1": formulario.contraseña.value,
+            "Clave2": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
